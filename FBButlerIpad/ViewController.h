721a7974d88480bc34114d71eb8c8732a2b42c4e
hungvo86 <hung.vo@okstate.edu>
2014-11-11 21:30:50 -0600

    Initial Commit

---
+++ FBButlerIpad/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FBButlerIpad
+//
+//  Created by Hung Vo on 11/11/14.
+//  Copyright (c) 2014 Hung Vo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FBButlerIpad/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FBButlerIpad
+//
+//  Created by Hung Vo on 11/11/14.
+//  Copyright (c) 2014 Hung Vo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
